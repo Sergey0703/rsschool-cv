02b2b12c9aa0744d052fe7a07fccbc75bd232d5b
Baliasnyi Serghii <sergey070373@gmail.com>
2021-08-22 21:37:24 +0300

    feat:image2

--- vertical-slider/assets/javascript/app.js
+++ vertical-slider/assets/javascript/app.js
@@ -1,13 +1,17 @@
 const upBtn = document.querySelector('.up-button');
 const downBtn = document.querySelector('.down-button');
 const container=document.querySelector('.container');
-const sidebar=document.querySelector('.left-slide');
+const slideBar=document.querySelector('.left-slide');
 const mainSlide = document.querySelector('.right-slide');
 const slidesCount=mainSlide.querySelectorAll('div').length;
 let activeSlideIndex=0;
+const animationTime = 500;
+//slideBar.style.top = `-100vh`
+//mainSlide.style.top = `-200vh`
+
 
 //sidebar.style.top=`-${slidesCount-1}vh`;
-sidebar.style.top=`-${(slidesCount-1)*100}vh`;
+slideBar.style.top=`-${(slidesCount-1)*100}vh`;
 upBtn.addEventListener('click',()=>{
   changeSlide('up');
 });
@@ -20,7 +24,9 @@ if(direction==='up'){
     activeSlideIndex++;
     if(activeSlideIndex===slidesCount){
         activeSlideIndex=0;
-    }
+      }
+
+
 }else if(direction==='down'){
     activeSlideIndex--;
     if(activeSlideIndex<0){
@@ -29,7 +35,7 @@ if(direction==='up'){
 }
 let height=container.clientHeight;
 mainSlide.style.transform=`translateY(-${height*activeSlideIndex}px)`;
-sidebar.style.transform=`translateY(${height*activeSlideIndex}px)`;
+slideBar.style.transform=`translateY(${height*activeSlideIndex}px)`;
 
 }
 
