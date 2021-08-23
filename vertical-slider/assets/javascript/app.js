97031a33c81a4520ded399770c77e58bf815088e
Baliasnyi Serghii <sergey070373@gmail.com>
2021-08-23 15:41:20 +0300

    feat:ok move

--- vertical-slider/assets/javascript/app.js
+++ vertical-slider/assets/javascript/app.js
@@ -3,15 +3,32 @@ const downBtn = document.querySelector('.down-button');
 const container=document.querySelector('.container');
 const slideBar=document.querySelector('.left-slide');
 const mainSlide = document.querySelector('.right-slide');
-const slidesCount=mainSlide.querySelectorAll('div').length;
+let height=container.clientHeight;
+const slidesCount=4;
 let activeSlideIndex=0;
-const animationTime = 500;
-//slideBar.style.top = `-100vh`
-//mainSlide.style.top = `-200vh`
+let onMove=false;
+
+container.addEventListener("transitionend",function(){
+    
+    if (activeSlideIndex===slidesCount){
+        activeSlideIndex=0;
+    }
+           
+    if(activeSlideIndex<0){
+        activeSlideIndex=slidesCount-1;
+    }
+        slideBar.style.transition=`none`;
+        mainSlide.style.transition=`none`;
+
+        slideBar.style.transform=`translateY(${height*activeSlideIndex}px)`;
+        mainSlide.style.transform=`translateY(-${height*activeSlideIndex}px)`;
+       
+      onMove=false;
+})
 
 
-//sidebar.style.top=`-${slidesCount-1}vh`;
-slideBar.style.top=`-${(slidesCount-1)*100}vh`;
+slideBar.style.top=`-${(slidesCount)*100}vh`;
+mainSlide.style.top=`-100vh`;
 upBtn.addEventListener('click',()=>{
   changeSlide('up');
 });
@@ -20,21 +37,33 @@ downBtn.addEventListener('click',()=>{
 });
 
 function changeSlide(direction){
+    if (onMove){
+        return;
+    }else{
+        onMove=true;
+    }
+   
 if(direction==='up'){
     activeSlideIndex++;
-    if(activeSlideIndex===slidesCount){
+    if(activeSlideIndex===slidesCount+1){
         activeSlideIndex=0;
-      }
+    }
 
 }else if(direction==='down'){
     activeSlideIndex--;
-    if(activeSlideIndex<0){
-        activeSlideIndex=slidesCount-1;
-    }
+    
 }
-let height=container.clientHeight;
-mainSlide.style.transform=`translateY(-${height*activeSlideIndex}px)`;
+
+slideBar.style.transition=`transform 0.5s ease-in-out`;
+mainSlide.style.transition=`transform 0.5s ease-in-out`;
+
 slideBar.style.transform=`translateY(${height*activeSlideIndex}px)`;
+
+if(activeSlideIndex<0){
+    mainSlide.style.transform=`translateY(${height}px)`;
+}else{
+    mainSlide.style.transform=`translateY(-${height*activeSlideIndex}px)`;   
+}
 
 }
 
