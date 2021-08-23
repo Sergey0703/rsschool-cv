435534dbcbf29dd77d242bb9a10a5923a505cf0d
Baliasnyi Serghii <sergey070373@gmail.com>
2021-08-23 23:13:46 +0300

    fix:sound

--- vertical-slider/assets/javascript/app.js
+++ vertical-slider/assets/javascript/app.js
@@ -3,10 +3,42 @@ const downBtn = document.querySelector('.down-button');
 const container=document.querySelector('.container');
 const slideBar=document.querySelector('.left-slide');
 const mainSlide = document.querySelector('.right-slide');
+const playButton= document.querySelector('.play-button');
+const textPlayButton= document.querySelector('#play');
+
+playButton.addEventListener('click', () => playSound());
+const arrSound=['/assets/sounds/Tropics-sound.mp3','/assets/sounds/Sea-sound.mp3','/assets/sounds/River-sound.mp3','/assets/sounds/Waterfall-sound.mp3'];
+
+
 let height=container.clientHeight;
 const slidesCount=4;
 let activeSlideIndex=0;
 let onMove=false;
+
+const audio = document.createElement('audio')
+//audio.loop = true;
+document.body.append(audio);
+let play = false;
+
+const playSound = () => {
+    audio.src=arrSound[activeSlideIndex];
+    play = !play
+    if (play) {
+        textPlayButton.className = 'fas fa-pause';
+        audio.play();
+        } else {
+        textPlayButton.className  = 'fas fa-play';
+        audio.pause();
+        }
+}
+
+const onResize = () => {
+    
+
+    slideBar.style.transform = `translateY(-${activeSlideIndex*height}px)`
+    mainSlide.style.transform = `translateY(${activeSlideIndex*height}px)`
+}
+window.addEventListener('resize', onResize)
 
 container.addEventListener("transitionend",function(){
     
@@ -24,6 +56,14 @@ container.addEventListener("transitionend",function(){
         mainSlide.style.transform=`translateY(-${height*activeSlideIndex}px)`;
        
       onMove=false;
+     
+      if(play){
+       if(audio.paused){
+        audio.src=arrSound[activeSlideIndex];
+        audio.load();
+        audio.play(); 
+        }
+      }
 })
 
 
@@ -42,7 +82,10 @@ function changeSlide(direction){
     }else{
         onMove=true;
     }
-   
+    if(play){
+    audio.pause();
+    }
+  
 if(direction==='up'){
     activeSlideIndex++;
     if(activeSlideIndex===slidesCount+1){
