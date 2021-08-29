3abdf13571c9b402626cd8f3d022e057b5ed405d
Baliasnyi Serghii <sergey070373@gmail.com>
2021-08-29 09:39:12 +0300

    fix:PrevNext

--- custom-video-player/assets/jscript/app.js
+++ custom-video-player/assets/jscript/app.js
@@ -4,27 +4,23 @@ const btnPrev=document.querySelector('.btn-rewind-prev');
 const btnMute=document.querySelector('.btn-volume');
 const btnVolume=document.querySelector('.progress-small');
 const btnScale=document.querySelector('.progress');
-
-const btnFull=document.querySelector('.btn-fullscr')
-
+const btnFull=document.querySelector('.btn-fullscr');
+const MAX_DURATION=1000;
 
 const video = document.querySelector('.viewer');
 const videoContainer=document.querySelector('.video-container');
 let videoScr=0;
 let videoArr=["/assets/video/video1.mp4","/assets/video/video2.mp4","/assets/video/video3.mp4","/assets/video/video4.mp4"];
-
+let posterArr=["/assets/img/vposter1.jpg","/assets/img/vposter2.jpg","/assets/img/vposter3.jpg","/assets/img/vposter4.jpg"];
 
 
 function playPause() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
     if (method === 'play') {
-        
         btnPlaySmall.classList.add('pause');
-        console.log('ppppp');
       }
       if (method === 'pause') {
-      
         btnPlaySmall.classList.remove('pause');
       }
   }
@@ -43,34 +39,36 @@ function playPause() {
 
 
   function toggleFullScr() {
-    console.log('tog');
+    
     if (!document.fullscreenElement) {
         videoContainer.requestFullscreen();
         btnFull.classList.add('btn-exit');
-        console.log('add');
+      
     } else {
       if (document.exitFullscreen) {
         document.exitFullscreen();
         btnFull.classList.remove('btn-exit');
-        console.log('rem');
+        
       }
     }
   }
   function scaleChange(){
-        video.currentTime = video.duration * (btnScale.value / 1000);
+        video.currentTime = video.duration * (btnScale.value / MAX_DURATION);
   }
 
   let videoProgress = () => {
-    btnScale.value = Math.round(video.currentTime / video.duration * 1000);
+    btnScale.value = Math.round(video.currentTime / video.duration * MAX_DURATION);
+    //console.log('btnScale.value=',btnScale.value)
+    if(btnScale.value==MAX_DURATION){
+        btnPlaySmall.classList.remove('pause');
+        //console.log('btnScale.value2=',btnScale.value)
+    }
   }
   function muteChange(){
-      //console.log('mute=',video.volume);
+      
     if(video.volume == 0) {
-
         video.volume = btnVolume.value;
         btnMute.classList.remove('btn-mute');
-     
-        
         } else {
         btnMute.classList.add('btn-mute');
         video.volume = 0;
@@ -84,18 +82,17 @@ function playPause() {
     if (evt.code === 'KeyF') toggleFullScr();
     
     if (evt.code === 'ArrowRight') {
-      video.currentTime += 5;
+      video.currentTime += 10;
     };
     if (evt.code === 'ArrowLeft') {
-      video.currentTime -= 5;
+      video.currentTime -= 10;
     };
    
-    
     if (evt.key === 'P' || (evt.key === 'Shift' && evt.key === 'p')) {
-      togglePrevHandler();
+      prevVideo();
     };
     if (evt.key === 'N' || (evt.key === 'Shift' && evt.key === 'n')) {
-      toggleNextHandler();
+      nextVideo();
     };
     
   }
@@ -114,19 +111,18 @@ function playPause() {
         videoScr=0;  
      }
      video.src=videoArr[videoScr];
+     video.poster=posterArr[videoScr];
      btnPlaySmall.classList.remove('pause');
      if(modeV===false){
      playPause();
      }
-   console.log('Next',videoScr);
-  }
+    }
 
   function prevVideo(){
       console.log('prev=',videoScr);
       let modeV=video.paused;
     if(modeV === false){
-   // playPause();
-    video['pause']();
+       video['pause']();
     }
    if (videoScr>=1){
       videoScr--;
@@ -134,15 +130,14 @@ function playPause() {
       videoScr=videoArr.length-1;  
    }
    video.src=videoArr[videoScr];
+   video.poster=posterArr[videoScr];
    btnPlaySmall.classList.remove('pause');
-   console.log('modeV=',modeV);
+   //console.log('modeV=',modeV);
    if(modeV===false){
     playPause();
-    console.log('playPause');
-   }
-   
- console.log('Next',videoScr);
-}
+    }
+  
+ }
 
 
   btnPlaySmall.addEventListener('click',playPause);
