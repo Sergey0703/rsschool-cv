4c9f78f23e288c95c1088ea5045875c38879dd60
Baliasnyi Serghii <sergey070373@gmail.com>
2021-09-26 23:44:07 +0300

    fix:contacts

---
+++ museum/assets/jscript/pictgallery.js
@@ -0,0 +1,3 @@
+const pictureInnerContainer = document.querySelector('.picture-inner-container');
+const img = `<img class="gallery-img" src="assets/img/galery/galery1.jpg" alt="galery1">`;
+pictureInnerContainer.innerHTML = img;
