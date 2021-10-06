d1524c9d154e6cc80d62e19a06bc80fa197c6a08
Baliasnyi Serghii <sergey070373@gmail.com>
2021-10-06 23:48:36 +0300

    fix:js

--- museum/assets/jscript/wgallery.js
+++ museum/assets/jscript/wgallery.js
@@ -1,4 +1,6 @@
 const wSliderButtonLeft=document.querySelector('.w-next-button');
+const wSliderButtonRight=document.querySelector('.w-prev-button');
+
 const wSlider=document.querySelector('w-slider');
 const wSliderItem=document.querySelector(".w-slider-item");
 const containerWelcome=document.querySelector('.container-welcome');
@@ -12,30 +14,39 @@ document.querySelector('.menu-open').addEventListener('click', function(){
 })
 
 const slidesCount=5;
-let slideIndex=0;
+let slideIndex=1;
+let transit=false;
 let elW = document.getElementById('wSlider');
-
+let slideWidth=parseInt(window.getComputedStyle(elW).width);
+wSliderItem.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
 
 
 wSliderButtonLeft.addEventListener('click',()=>changeSlide('left'));
+wSliderButtonRight.addEventListener('click',()=>changeSlide('right'));
 wSliderItem.addEventListener("transitionend",function(){
     let slideWidth=parseInt(window.getComputedStyle(elW).width); 
-    if (slideIndex===slidesCount){
+    console.log('transitioned=',slideIndex);
+    if(slideIndex===0){
+      slideIndex=slidesCount;
+      console.log('slideIndex=slidesCount-1');
+   }else if (slideIndex===slidesCount){
         slideIndex=0;
+        console.log('slideIndex=0');
     }
            
-      if(slideIndex<0){
-        activeSlideIndex=slidesCount-1;
-     }
+     
      //   slideBar.style.transition=`none`;
      wSliderItem.style.transition=`none`;
 
       wSliderItem.style.transform=`translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
+      transit=false;
       //  mainSlide.style.transform=`translateY(-${height*activeSlideIndex}px)`;
        
 });
 
 function changeSlide(direction){
+if(transit===true) return;
+  transit=true;
 //console.log('left!');
 //containerWelcome.classList.remove('vcontrols_active');
 let slideWidth=parseInt(window.getComputedStyle(elW).width);
@@ -47,8 +58,10 @@ if(direction==='left'){
 }else if(direction==='right'){
     slideIndex--;
 }
+console.log('index=',slideIndex);
 wSliderItem.style.transition = 'transform .5s';
 wSliderItem.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
+ //transit=false;
 }
 
 export default changeSlide;
