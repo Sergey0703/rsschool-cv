df2ad110d79724fc8046e0c183c3e5e40cd04ab2
Baliasnyi Serghii <sergey070373@gmail.com>
2021-10-10 21:22:03 +0300

    fix:dom6

--- museum/assets/jscript/map.js
+++ museum/assets/jscript/map.js
@@ -1,28 +1,30 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2VyZ2V5MDcwMyIsImEiOiJja3VrMG55bHozM2dyMnVtb25tYXF2YTRnIn0.tT-gIguccIT1rI2He_wJEQ';
 const map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
+style: 'mapbox://styles/mapbox/light-v10',
 center: [2.3364, 48.86091],
-zoom: 9
+zoom: 16
 });
- 
+const marker1 = new mapboxgl.Marker({ color: 'black', rotation: 0 })
+.setLngLat([2.3364, 48.86091])
+.addTo(map);
 // Create a default Marker and add it to the map.
-const marker1 = new mapboxgl.Marker()
+const marker2 = new mapboxgl.Marker({ color: 'grey', rotation: 0 })
 .setLngLat([2.3333, 48.8602])
 .addTo(map);
  
 // Create a default Marker, colored black, rotated 45 degrees.
-const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+const marker3 = new mapboxgl.Marker({ color: 'grey', rotation: 0 })
 .setLngLat([2.3397, 48.8607])
 .addTo(map);
 
 // Create a default Marker, colored black, rotated 45 degrees.
-const marker3 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+const marker4 = new mapboxgl.Marker({ color: 'grey', rotation: 0 })
 .setLngLat([2.3330, 48.8619])
 .addTo(map);
 
 // Create a default Marker, colored black, rotated 45 degrees.
-const marker4 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+const marker5 = new mapboxgl.Marker({ color: 'grey', rotation: 0 })
 .setLngLat([2.3365, 48.8625])
 .addTo(map);
 
