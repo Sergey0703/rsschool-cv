496b73ea6d9ebaffad13638706216c9b3fb15755
Baliasnyi Serghii <sergey070373@gmail.com>
2021-10-19 22:22:45 +0300

    init:dateTime

---
+++ momentum/assets/js/datetime.js
@@ -0,0 +1,22 @@
+const time=document.querySelector('.time');
+const date=document.querySelector('.date');
+
+let showDate =function(){
+    const dat=new Date();
+    let options={weekday: 'long', month: 'long', day:'numeric'};
+    const currentDate= dat.toLocaleDateString('en-En',options);
+
+    date.textContent=currentDate;
+}
+
+function showTime(){
+    const date=new Date();
+    const currentTime=date.toLocaleTimeString();
+    time.textContent=currentTime;
+    console.log('show=',currentTime);
+    showDate();
+    setTimeout(showTime,1000);
+}
+
+showTime();
+export default time;
--- momentum/assets/js/index.js
+++ momentum/assets/js/index.js
@@ -1,3 +1,4 @@
 import setBg from './slider.js';
+import time from './datetime.js';
 
 setBg();
