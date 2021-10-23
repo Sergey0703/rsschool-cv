17828a5ce2c4d8da8de6dffc2c805400bef97295
Baliasnyi Serghii <sergey070373@gmail.com>
2021-10-24 00:26:43 +0300

    fix:player2

--- momentum/assets/js/playList.js
+++ momentum/assets/js/playList.js
@@ -8,6 +8,16 @@ const playList = [
       title: 'River Flows In You',
       src: '../momentum/assets/sounds/River Flows In You.mp3',
       duration: '03:50'
-    }
+    },
+    {      
+        title: 'Ennio Morricone',
+        src: '../momentum/assets/sounds/Ennio Morricone.mp3',
+        duration: '01:37'
+      },
+      {      
+        title: 'Summer Wind',
+        src: '../momentum/assets/sounds/Summer Wind.mp3',
+        duration: '01:50'
+      }
   ]
   export default playList;
