a5704126163d67a7cb6ceafb4bcf024ccfc8c7e6
Baliasnyi Serghii <sergey070373@gmail.com>
2021-10-24 21:22:00 +0300

    fix:lang2

--- momentum/assets/js/tweak.js
+++ momentum/assets/js/tweak.js
@@ -1,5 +1,6 @@
 import getQuotes from "./quotes.js";
 import getWeather from "./weather.js";
+const inpName=document.querySelector('.name');
 
 const langBtn=document.querySelector('#choose-lang-ru');
 let lang='en';
@@ -7,12 +8,14 @@ let lang='en';
 function chooseLang(){
     if(lang==='en'){
         lang='ru';
-        
+        inpName.placeholder="Введите ваше имя";
     }else{
         lang='en';
+        inpName.placeholder="Enter you name";
     }
     console.log('lang=',lang);
 langBtn.classList.toggle('choose-lang');
+
 getQuotes(lang);
 getWeather();
 }
